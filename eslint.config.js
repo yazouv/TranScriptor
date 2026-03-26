7199f8955506b5d864c505a2ee4d1915470488c3
Yazouv <yazouv.pro@gmail.com>
2026-03-26 21:45:43 +0100

    chore: initial project setup (tsconfig, eslint, vitest, package.json)

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'no-console': 'warn',
+    },
+  },
+];
